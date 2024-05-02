d212b8e9df8c96cf8caf4a44e6f643edcdf126f6
HardikRuby <hardik.u.purohit@gmail.com>
2024-05-02 19:53:55 +0530

    Added changes to resolve rubocop offenses

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,40 +8,40 @@ describe StringCalculator do
         expect { described_class.add('1') }.to_not raise_error
         expect { described_class.add(1) }.to raise_error(RuntimeError, 'Argument must be a string')
       end
-      
+
       it 'expects only one argument' do
         expect { described_class.add(1, 2) }.to raise_error(ArgumentError)
       end
     end
 
     context 'when string id blank' do
-      it "returns 0" do
-        expect(described_class.add("")).to eq 0 
+      it 'returns 0' do
+        expect(described_class.add('')).to eq 0
       end
     end
 
     context 'when valid input passed' do
       it 'returns sum of numbers that are passed as string' do
-        expect(described_class.add("1")).to eq 1
-        expect(described_class.add("1,5")).to eq 6
+        expect(described_class.add('1')).to eq 1
+        expect(described_class.add('1,5')).to eq 6
       end
     end
 
-    context "when input string contains new line character" do
+    context 'when input string contains new line character' do
       it 'returns sum of passed numbers' do
         expect(described_class.add("1\n2,3")).to eq 6
       end
     end
 
-    context "when string has different delimiter" do
-      it "considers delimiter and sum number" do
+    context 'when string has different delimiter' do
+      it 'considers delimiter and sum number' do
         expect(described_class.add("//;\n1;2")).to eq 3
       end
     end
 
-    context "when input string contains negative numbers" do
-      it "should raise exception with message" do
-        expect { described_class.add("1,-2,") }.to raise_error(RuntimeError, "Negative numbers not allowed: -2")
+    context 'when input string contains negative numbers' do
+      it 'should raise exception with message' do
+        expect { described_class.add('1,-2,') }.to raise_error(RuntimeError, 'Negative numbers not allowed: -2')
       end
     end
   end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,37 +1,42 @@
 # frozen_string_literal: true
+
 require 'pry'
 
+# This class is responsible to return addition of input string containing numbers
 class StringCalculator
   def self.add(string_of_numbers)
     raise 'Argument must be a string' unless string_of_numbers.is_a?(String)
 
-    return 0 if string_of_numbers.length == 0
+    return 0 if string_of_numbers.empty?
+
     delimiter = find_delimiter(string_of_numbers)
     numbers = extract_numbers(string_of_numbers, delimiter)
     check_for_negatives(numbers)
     numbers.sum
   end
 
-  private
+  class << self
+    private
 
-  # find differnt delimiters
-  def self.find_delimiter(string_of_numbers)
-    delimiter = ","
-    if string_of_numbers.start_with?("//")
-      delimiter = string_of_numbers[2]
+    # find differnt delimiters
+    def find_delimiter(string_of_numbers)
+      delimiter = ','
+      if string_of_numbers.start_with?('//')
+        delimiter = string_of_numbers[2]
+      end
+      delimiter
     end
-    delimiter
-  end
 
-  # Extracts numbers from the string
-  def self.extract_numbers(string_of_numbers, delimiter)
-    string_of_numbers = string_of_numbers.gsub(/\/\/.*\n/, '').gsub(/(?<=\d)\n(?=\d)/, delimiter)
-    string_of_numbers.split(Regexp.new(delimiter)).map(&:to_i)
-  end
+    # Extracts numbers from the string
+    def extract_numbers(string_of_numbers, delimiter)
+      string_of_numbers = string_of_numbers.gsub(%r{/\.*\n/}, '').gsub(/(?<=\d)\n(?=\d)/, delimiter)
+      string_of_numbers.split(Regexp.new(delimiter)).map(&:to_i)
+    end
 
-  # Checks for negative numbers and raises an exception if any are found
-  def self.check_for_negatives(numbers)
-    negatives = numbers.select { |num| num < 0 }
-    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+    # Checks for negative numbers and raises an exception if any are found
+    def check_for_negatives(numbers)
+      negatives = numbers.select(&:negative?)
+      raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+    end
   end
 end
